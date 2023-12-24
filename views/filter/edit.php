dc7f7934d837f5c484c3fb5490d833238f64cdd1
jctneko <johnson780510@gmail.com>
2023-12-25 05:28:47 +0800

    1.調整filter介面語言

--- views/filter/edit.php
+++ views/filter/edit.php
@@ -3,11 +3,14 @@
 <script src="<?= $this->asset('filter/list.js') ?>"></script>
 
 <div class="container">
-    <h2>編輯過濾詞</h2>
+    <h2>名前フィルター</h2>
     <form id="edit-filter-form">
         <div class="form-group">
             <textarea class="form-control" id="filter-words" name="filter_words" rows="10"></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">保存過濾詞</button>
+        <div class="alert alert-info" role="alert">
+            半角、全角特殊記号(!@#$%^&*,.?)は既に禁止されていますので、リストに特殊記号を追加する必要はありません。
+        </div>
+        <button type="submit" class="btn btn-primary">保存</button>
     </form>
 </div>
